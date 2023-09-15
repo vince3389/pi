ae8d0e23279bd59071c83085d1280f9dc508e74a
vince3389 <109995934+vince3389@users.noreply.github.com>
2023-09-15 13:29:32 -0400

    new upd

---
+++ Alarmpi.py
@@ -0,0 +1,54 @@
+
+#!/usr/bin/env python3
+
+### NOT WORKING AS INTENDED YET
+
+### CALLED WITH SensorPi_V05....
+
+##############################################################################
+
+import time
+import RPi.GPIO as GPIO
+import paho.mqtt.client as mqtt
+
+# MQTT broker configuration
+mqtt_broker = "10.1.1.16"  # Change to the IP address of your MQTT broker
+mqtt_port = 1883
+mqtt_username = "homeassistant"  # Replace with your MQTT username
+mqtt_password = "ul2aip8oe4kuu5ooruf4ishah1naig5ae8ookaich5iech9Ied2ookoonuquaiGh"  # Replace with your MQTT password
+mqtt_topic = "home/sensors"  # MQTT topic for publishing sensor readings
+
+# GPIO Configuration
+reed_pins = [5, 26, 16, 20, 21]  # Replace with the actual GPIO pins connected to the reed sensors
+pir_pins = [6, 13, 19]  # Replace with the actual GPIO pins connected to the PIR sensors
+
+GPIO.setmode(GPIO.BCM)
+for pin in reed_pins:
+    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+for pin in pir_pins:
+    GPIO.setup(pin, GPIO.IN)
+
+# Set up MQTT client
+client = mqtt.Client()
+client.username_pw_set(mqtt_username, mqtt_password)
+client.connect(mqtt_broker, mqtt_port, 60)
+
+while True:
+    # Read reed sensor states
+    reed_states = [GPIO.input(pin) for pin in reed_pins]
+
+    # Read PIR sensor states
+    pir_states = [GPIO.input(pin) for pin in pir_pins]
+
+    # Publish reed sensor states via MQTT
+    for i, state in enumerate(reed_states):
+        client.publish(f"{mqtt_topic}/reed_sensor_{i+1}_state", state)
+
+    # Publish PIR sensor states via MQTT
+    for i, state in enumerate(pir_states):
+        client.publish(f"{mqtt_topic}/pir_sensor_{i+1}_state", state)
+
+    time.sleep(1)  # Publish the data every 1 second
+
+# Clean up GPIO
+GPIO.cleanup()
---
+++ SensorsPi_V05-2023.py
@@ -0,0 +1,114 @@
+
+#!/bin/bash
+############################################################
+####### SCRIPT LAST VERSION MAY 30TH 2023 11H33  ############
+############################################################
+# BY VINCENT CHARRON
+
+#Set up MQTT broker:
+
+#Install and configure an MQTT broker on the Raspberry Pi. Mosquitto is a popula                                                               r MQTT broker that you can install by running the command:
+#       sudo apt-get install mosquitto mosquitto-clients
+#Start the Mosquitto service:
+#       sudo systemctl start mosquitto
+#You may also want to enable the Mosquitto service to start on boot:
+#       sudo systemctl enable mosquitto
+
+#Install necessary libraries:
+#   Install the w1thermsensor library for reading the temperature from the DS18B                                                               20 sensor by running:
+#       sudo pip install w1thermsensor
+#   Install the paho-mqtt library for MQTT communication by running:
+#       sudo pip install paho-mqtt
+
+#Create a Python script:
+
+# nano SensorsPi_V05-2023.py
+# chmod +x script.py
+
+
+import time
+import json
+import Adafruit_DHT
+import RPi.GPIO as GPIO
+from w1thermsensor import W1ThermSensor
+import paho.mqtt.client as mqtt
+
+import subprocess
+
+# Define the command to run the script in the background
+command1 = ["python3", "/home/sensor/Alarmpi.py", "&"]
+command2 = ["python3", "/home/sensor/Pms5003.py", "&"]
+command3 = ["python3", "/home/sensor/DHT11.py", "&"]
+
+# Run the command in the background
+subprocess.Popen(command1)
+subprocess.Popen(command2)
+subprocess.Popen(command3)
+
+#MQTT broker configuration
+mqtt_broker = "10.1.1.16"  # Change to the IP address of your MQTT broker
+mqtt_port = 1883
+mqtt_username = "homeassistant"  # Replace with your MQTT username
+mqtt_password = "ul2aip8oe4kuu5ooruf4ishah1naig5ae8ookaich5iech9Ied2ookoonuquaiG                                                               h"  # Replace with your MQTT password
+mqtt_topic = "home/sensors"  # MQTT topic for publishing sensor readings
+
+# Connect to the MQTT broker with authentication
+client = mqtt.Client()
+client.username_pw_set(mqtt_username, mqtt_password)
+client.connect(mqtt_broker, mqtt_port, 60)
+
+# Publish sensor readings
+def publish_sensor_reading(sensor_name, sensor_value):
+    payload = {
+        "sensor_name": sensor_name,
+        "sensor_value": sensor_value
+    }
+    mqtt_topic_with_sensor = f"{mqtt_topic}/{sensor_name}"
+    client.publish(mqtt_topic_with_sensor, json.dumps(payload))
+
+# DHT11 Sensor Configuration
+dht_sensors = [
+    {"name": "dht11_1", "pin": 22},
+    {"name": "dht11_2", "pin": 17},
+    {"name": "dht11_3", "pin": 27}
+]
+
+# DS18B20 Sensor Configuration
+ds18b20_sensor = W1ThermSensor()
+
+
+while True:
+    # Read DHT11 sensors
+#    for sensor in dht_sensors:
+ #       humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, sensor["pin"])
+  #      if humidity is not None and temperature is not None:
+   #         publish_sensor_reading(sensor["name"] + "_temperature", round(temperature, 2))
+    #        publish_sensor_reading(sensor["name"] + "_humidity", round(humidity, 2))
+
+    # Read DS18B20 sensor * un comment for Celsius
+    ds18b20_temperature = ds18b20_sensor.get_temperature()
+
+    #MY Test Farenheit * comment for celsius
+   # ds18b20_temperature = round(ds18b20_sensor.get_temperature() * 9/5 + 32, 2)
+
+    publish_sensor_reading("ds18b20_temperature", round(ds18b20_temperature, 2))
+
+    time.sleep(5)  # Publish the data every 5 seconds
+
+
+
+
+
+
+
+
+
+
+# Make sure the script runs in the background at restart by adding it here:
+# sudo nano /etc/rc.local
+# add a line with:
+# python3 /home/sensor/SensorsPi_V05-2023.py
+
+
+
+### Finaly I had to create the script as a service to be able to run in at boot                                                                in the background
